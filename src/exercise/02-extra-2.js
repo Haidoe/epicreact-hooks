c5e76f8a7efb8b415642e090d61aedb0735fab46
haidoe <haidren.amalia@gmail.com>
2022-11-13 22:58:46 -0800

    Exercise 2 - Extra Credit 2 - useEffect Array Dependecies

---
+++ src/exercise/02-extra-2.js
@@ -0,0 +1,35 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+
+function Greeting({initialName = ''}) {
+  //This will make the window.localStorage.getItem run only once in every re-render
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
+  )
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialname="Heids" />
+}
+
+export default App
